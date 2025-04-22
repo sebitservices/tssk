e2f74f23855007e32676033de4ae67b0c69f8c7c
sebitservices <admin@sebitservices.cl>
2025-04-21 22:43:36 -0400

    fix: Configuración de webpack para manejar imágenes y archivos estáticos

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '',
+    publicPath: '/',
     clean: true
   },
   module: {
@@ -42,6 +43,17 @@ module.exports = {
       template: './public/index.html',
       filename: 'index.html',
       inject: true
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          to: '',
+          globOptions: {
+            ignore: ['**/index.html']
+          }
+        }
+      ]
     })
   ],
   devServer: {
